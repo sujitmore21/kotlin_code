373f3515002f9c9430b8ad7e266916d62f5f233c
Sujit More <sujitmore21@gmail.com>
2019-07-01 12:48:04 +0530

    operator: string comparison

---
+++ src/operators/String_comparison.kt
@@ -0,0 +1,14 @@
+package operators
+
+fun main(args: Array<String>){
+
+    var str1 :String = "Sujit"
+    var str2 :String = "Sujit"
+    //var str2 :String = "sujit"
+
+    if (str1 == str2)
+        println("same")
+    else
+        println("Not same")
+
+}
