82d1f6d1c7141c73c442dbe0035b5f4c8ea86ad8
Sujit More <sujitmore21@gmail.com>
2019-07-01 13:21:42 +0530

    null handling

---
+++ src/operators/null_handling.kt
@@ -0,0 +1,15 @@
+package operators
+
+import class_object.aliens
+
+
+fun main(args: Array<String>){
+
+    var str :String? = null
+    var sujit : aliens? = aliens()
+
+    sujit = null
+
+    println(sujit?.name)
+
+}
