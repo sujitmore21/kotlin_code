a1cfe6fc5839c7c67f8a8fbb438b8038c0a0355c
Sujit More <sujitmore21@gmail.com>
2019-08-05 10:59:23 +0530

    how to use Try Catch and while loop

---
+++ src/operators/WhileLoop.kt
@@ -0,0 +1,22 @@
+package operators
+
+fun main(args:Array<String>){
+    var count=1
+    while(count<=5) {
+        println("enter the loop")
+        // block of code
+        if(count==2) {
+            println("count:$count")
+        }
+
+
+        count++
+
+    }
+
+    println("loop done")
+
+
+
+}
+
